b9a3fab9f06b973a40af7daa1643c24adf597a79
apetrescu <apetrescu@seibert-media.net>
2020-03-21 18:06:49 +0100

    initial

---
+++ RSub1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RSub1
+//
+//  Created by Alexandru Petrescu on 21.03.20.
+//  Copyright © 2020 RSub1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
